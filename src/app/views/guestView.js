0f3f38ba00dcdced86cebd49160398d186aef741
Neo Nguyen <neon@kth.se>
2023-11-28 21:33:49 +0100

    added logos to button on guestView and have added elements to upcoming page

--- src/app/views/guestView.js
+++ src/app/views/guestView.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 function GuestView() {
     return (
         <div className="flex flex-col h-screen">
@@ -5,11 +7,27 @@ function GuestView() {
                 <h1 className="text-6xl font-bold text-white sm:text-8xl text-center">The Coffin</h1>
                 <h2 className="text-4xl text-white sm:text-6xl">Welcome to Kistan 2.0 </h2>
                 <div className="py-5 grid gap-y-5 gap-x-10 grid-cols-1 sm:grid-cols-2">
-                    <button className="transition rounded-lg font-bold bg-red-700 hover:bg-red-800 text-white btn-lg py-4">QMISK</button>
-                    <button className="transition rounded-lg font-bold bg-blue-700 hover:bg-blue-800 text-white btn-lg py-4">TMEIT</button>
+                    <button className="flex items-center justify-center transition rounded-lg bg-red-700 hover:bg-red-800 btn-lg py-4" onClick={() => window.location.href = "https://qmisk.com/"}>
+                        <Image
+                            alt="Qmisk logo"
+                            src="/Qmisk-white.png"
+                            width="676"
+                            height="231"
+                            className="w-56"
+                        />
+                    </button>
+                    <button className="flex items-center justify-center transition rounded-lg bg-blue-700 hover:bg-blue-800 btn-lg py-4" onClick={() => window.location.href = "https://tmeit.se/"}>
+                        <Image
+                        alt="Tmeit logo"
+                        src="/Tmeit.svg"
+                        width="676"
+                        height="231"
+                        className="w-44"
+                        />    
+                    </button>
                 </div>
                 <div className="text-center">
-                    <button className="transition rounded-lg font-bold bg-gray-700 hover:bg-gray-800 text-white btn-lg px-10 py-4" onClick={() => window.location.href="upcoming"}>Upcoming</button>
+                    <button className="transition rounded-lg font-bold bg-gray-700 hover:bg-gray-800 text-white btn-lg px-10 py-4" onClick={() => window.location.href = "upcoming"}>Upcoming</button>
                 </div>
             </div>
             <footer>
