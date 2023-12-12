d634709aabc28b4ba03e80fcbba964d4ee788128
Roy Evert Andrej Dolonius <124092984+Gigglinglizard@users.noreply.github.com>
2023-12-12 22:17:04 +0100

    developed guest list functionality, still needs to be connected and persist

--- src/app/views/livePubView.jsx
+++ src/app/views/livePubView.jsx
@@ -1,10 +1,52 @@
+import {useState} from 'react';
+
 export default function LivePub(props){
+    const[guest, setGuest] = useState('');
+    const[guestsArray, setGuestsArray] = useState([]);
+    const[totalGuests, setTotalGuests] = useState(0);
+    
+    const inputChange = (e) => {
+        setGuest(e.target.value);
+    };
+
+    const inputSubmit = (e) => {
+        e.preventDefault();
+        if (guest.trim()){
+            setGuestsArray([...guestsArray, guest]);
+            setGuest('');
+            setTotalGuests(totalGuests + 1);
+        }
+    };
+
+    const handleDelete = (index) => {
+        setGuestsArray(guestsArray.filter((_,i) => i !== index));
+        setTotalGuests(totalGuests - 1);
+    };
+
+
+
     return(
-        <div>Insert Pub Name Here
-            <hr></hr>
-            <input type = "text"></input>    
-            <button className = "simpleButton">Add Internal</button>   
-            <button className = "simpleButton">Add External</button>    
+        <div>
+            <h1 className="header">Guest List</h1>
+            <p>Total Guests: {totalGuests}</p>
+            <form onSubmit={inputSubmit}>
+                <input 
+                type = "text" 
+                value={guest} 
+                onChange={inputChange} 
+                placeholder="Add new guest here">
+                </input> 
+                <button className = "simpleButton">Add Internal</button>   
+                <button className = "simpleButton">Add External</button>  
+            </form>
+            <ul>
+                {guestsArray.map((guest, index) => (
+                    <li key={index}>
+                        {guest}
+                        <button className="simpleButton" onClick={() => handleDelete(index)}>x</button>
+                    </li>
+                ))}
+            </ul>
         </div>
         
     );
