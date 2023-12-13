a4f1014a66fd5eb01900a9b76293d5488e3d4051
Roy Evert Andrej Dolonius <124092984+Gigglinglizard@users.noreply.github.com>
2023-12-13 20:21:12 +0100

    Guest List functionality expanded, base functionality works as intended, routing left to be done for beta version

--- src/app/live/livePubView.jsx
+++ src/app/live/livePubView.jsx
@@ -4,7 +4,10 @@ export default function LivePub(props){
     const[guest, setGuest] = useState('');
     const[guestsArray, setGuestsArray] = useState([]);
     const[totalGuests, setTotalGuests] = useState(0);
-    
+    const [showExternalInput, setShowExternalInput] = useState(false);
+    const [externalInputValue, setExternalInputValue] = useState('');
+    const [activeInternalGuestIndex, setActiveInternalGuestIndex] = useState(null);
+
     const inputChange = (e) => {
         setGuest(e.target.value);
     };
@@ -12,23 +15,53 @@ export default function LivePub(props){
     const inputSubmit = (e) => {
         e.preventDefault();
         if (guest.trim()){
-            setGuestsArray([...guestsArray, guest]);
+            setGuestsArray([...guestsArray, {internal: guest, external: null}]);
             setGuest('');
             setTotalGuests(totalGuests + 1);
         }
     };
 
     const handleDelete = (index) => {
-        setGuestsArray(guestsArray.filter((_,i) => i !== index));
+        const updatedGuestsArray = [...guestsArray];
+
+        if (updatedGuestsArray[index].external) {
+            updatedGuestsArray[index].external = null;
+        } else {
+            updatedGuestsArray.splice(index, 1);
+        }
+    
+        setGuestsArray(updatedGuestsArray);
         setTotalGuests(totalGuests - 1);
     };
 
+    const renderExternalInput = (index) => {
+        setShowExternalInput(true);
+        setActiveInternalGuestIndex(index);
+    };
 
+    const handleExternalInputChange = (e) => {
+        setExternalInputValue(e.target.value);
+    };
+
+    const handleExternalInputSubmit = () => {
+        if (externalInputValue.trim()) {
+            const updatedGuestsArray = [...guestsArray];
+            updatedGuestsArray[activeInternalGuestIndex] = {
+              ...updatedGuestsArray[activeInternalGuestIndex],
+              external: externalInputValue,
+            };
+            setGuestsArray(updatedGuestsArray);
+            setTotalGuests(totalGuests + 1);
+            setExternalInputValue('');
+            setShowExternalInput(false);
+            setActiveInternalGuestIndex(null); 
+        }
+    };
 
     return(
-        <div>
-            <h1 className="header">Guest List</h1>
-            <p>Total Guests: {totalGuests}</p>
+        <div className="bg-gray-800 min-h-screen">
+            <h1 className="font-bold text-center text-5xl">Guest List</h1>
+            <p className="font-bold text-3xl">Total Guests: {totalGuests}</p>
             <form onSubmit={inputSubmit}>
                 <input 
                 type = "text" 
@@ -36,18 +69,30 @@ export default function LivePub(props){
                 onChange={inputChange} 
                 placeholder="Add new guest here">
                 </input> 
-                <button className = "simpleButton">Add Internal</button>   
-                <button className = "simpleButton">Add External</button>  
+                <button className = "simpleButton">Add Internal</button>    
             </form>
-            <ul>
+            <ul className='list-none text-center'>
                 {guestsArray.map((guest, index) => (
-                    <li key={index}>
-                        {guest}
-                        <button className="simpleButton" onClick={() => handleDelete(index)}>x</button>
+                    <li key={index} className='bg-gray-950 flex font-bold justify-left'>
+                        <button className="bg-red-500 hover:bg-red-700 text-white font-bold px-4 py-2 m-2 rounded w-6 flex justify-center" onClick={() => handleDelete(index)}>x</button>
+                        <button onClick={() => renderExternalInput(index)} className={`bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 m-2 rounded w-6 flex justify-center ${guest.external ? 'cursor-not-allowed opacity-50' : ''}`} disabled={guest.external !==null}>+1</button>
+                        <p className='flex items-center justify-left m-2'>{guest.internal}</p>
+                        {guest.external && (
+                        <div className="flex items-center justify-center mx-auto">
+                            <button className="bg-red-500 hover:bg-red-700 text-white font-bold px-4 py-2 m-2 rounded" onClick={() => handleDelete(index)}>x</button>
+                            <div>+1: </div>
+                            <div className="m-2">{guest.external}</div>
+                        </div>
+                        )}
                     </li>
                 ))}
             </ul>
+            {showExternalInput && (
+            <div className="flex items-center justify-left m-2">
+                <input onChange={handleExternalInputChange} value={externalInputValue} placeholder="Add +1" className="text-black h- m-2 rounded-full justify-center"/>
+                <button onClick={handleExternalInputSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 m-2 rounded">Add +1</button>
+            </div>)}
+            {console.log(guestsArray)}
         </div>
-        
     );
 }
