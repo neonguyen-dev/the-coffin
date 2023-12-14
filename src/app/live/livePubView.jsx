1eb08b068683bcc6df15990061d03f04e14d9fb3
Roy Evert Andrej Dolonius <124092984+Gigglinglizard@users.noreply.github.com>
2023-12-14 17:31:21 +0100

    adjusted some styling

--- src/app/live/livePubView.jsx
+++ src/app/live/livePubView.jsx
@@ -65,7 +65,7 @@ export default function LivePub(props){
     };
 
     return(
-        <div className="bg-gray-800 min-h-screen">
+        <div className="bg-gray-800 min-h-screen p-10">
             <h1 className="font-bold text-center text-5xl">Guest List</h1>
             <p className="font-bold text-3xl">Total Guests: {totalGuests}</p>
             <p className="font-bold text-3xl">Total Internal Guests: {internals}</p>
@@ -79,9 +79,14 @@ export default function LivePub(props){
                 </input> 
                 <button className = "simpleButton">Add Internal</button>    
             </form>
+            {showExternalInput && (
+            <form className="flex items-center justify-left m-2">
+                <input onChange={handleExternalInputChange} value={externalInputValue} placeholder="Add +1" className="px-4 py-2 text-black h- m-2 rounded-full justify-center"/>
+                <button onClick={handleExternalInputSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 w-auto m-2 rounded">Add +1</button>
+            </form>)}
             <ul className='list-none text-center'>
                 {guestsArray.map((guest, index) => (
-                    <li key={index} className='bg-gray-950 flex font-bold justify-left'>
+                    <li key={index} className='bg-gray-950 flex font-bold justify-left border'>
                         <button className="bg-red-500 hover:bg-red-700 text-white font-bold px-4 py-2 m-2 rounded w-6 flex justify-center" onClick={() => handleDelete(index)}>x</button>
                         <button onClick={() => renderExternalInput(index)} className={`bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 m-2 rounded w-6 flex justify-center ${guest.external ? 'cursor-not-allowed opacity-50' : ''}`} disabled={guest.external !==null}>+1</button>
                         <p className='flex items-center justify-left m-2'>{guest.internal}</p>
@@ -95,11 +100,7 @@ export default function LivePub(props){
                     </li>
                 ))}
             </ul>
-            {showExternalInput && (
-            <form className="flex items-center justify-left m-2">
-                <input onChange={handleExternalInputChange} value={externalInputValue} placeholder="Add +1" className="text-black h- m-2 rounded-full justify-center"/>
-                <button onClick={handleExternalInputSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 m-2 rounded">Add +1</button>
-            </form>)}
+
             {console.log(guestsArray)}
         </div>
     );
