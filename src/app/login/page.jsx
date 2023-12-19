676ec61e6e9310765d3cbfe6a94b9ecfd3f4218e
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-12-19 23:50:06 +0100

    Moved presenter logic to page file for guest and login to solve error after logging in

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,29 @@
-import Login from "./loginPresenter.jsx"
+"use client"
 
-export default 
-function Home() {
-  return <Login />
-}
+// Third party library imports
+import { observer } from "mobx-react-lite";
+import { useState } from "react";
+
+// Local imports from project
+import { resolveAuthSignIn } from "@/firebase/firebaseAuth";
+import LoginView from "./loginView";
+
+export default
+observer(
+  function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    return <LoginView
+      setEmail={setEmail}
+      setPassword={setPassword}
+      login={() => {
+        resolveAuthSignIn(email, password).then((user) => {
+          console.log(user);
+          window.location.href = "/";
+        }).catch((error) => {
+          console.log(error);
+        })
+      }}/>
+  }
+);
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,22 @@
-import Guest from "./guestPresenter.jsx"
+'use client'
 
-export default function Home() {
-  return (
-    <Guest/>
-  )
-}
+// Third party library imports
+import { resolveAuthSignOut } from "@/firebase/firebaseAuth.js";
+import { observer } from "mobx-react-lite";
+
+// Local imports from project
+import GuestView from "./guestView.jsx";
+
+export default observer(
+  function Guest() {
+    return (
+      <div>
+        <GuestView logout={() => {
+          resolveAuthSignOut().catch((error) => {
+            console.log(error);
+          })
+        }}/>
+      </div>
+    );
+  }
+);
