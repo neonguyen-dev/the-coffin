ff708855a2497735f0ba534d48d8a318b7532042
Roy Evert Andrej Dolonius <124092984+Gigglinglizard@users.noreply.github.com>
2023-12-20 13:54:56 +0100

    /live now complies with MVP model

--- src/app/live/livePubPresenter.jsx
+++ src/app/live/livePubPresenter.jsx
@@ -2,9 +2,90 @@
 
 import LivePub from "./livePubView.jsx"; 
 import { observer } from "mobx-react-lite";
+import {useState} from 'react';
 
 export default observer(
     function CreateLivePub() {
-        return <LivePub/>;
+        const[guest, setGuest] = useState('');
+        const[guestsArray, setGuestsArray] = useState([]);
+        const[internals, setInternals] = useState(0);
+        const[externals, setExternals] = useState(0);
+        const[totalGuests, setTotalGuests] = useState(0);
+        const [showExternalInput, setShowExternalInput] = useState(false);
+        const [externalInputValue, setExternalInputValue] = useState('');
+        const [activeInternalGuestIndex, setActiveInternalGuestIndex] = useState(null);
+    
+        const inputChange = (e) => {
+            setGuest(e.target.value);
+        };
+    
+        const inputSubmit = (e) => {
+            e.preventDefault();
+            if (guest.trim()){
+                setGuestsArray([...guestsArray, {internal: guest, external: null}]);
+                setGuest('');
+                setTotalGuests(totalGuests + 1);
+                setInternals(internals + 1);
+            }
+        };
+    
+        const handleDelete = (index) => {
+            const updatedGuestsArray = [...guestsArray];
+    
+            if (updatedGuestsArray[index].external) {
+                updatedGuestsArray[index].external = null;
+                setExternals(externals -1);
+            } else {
+                updatedGuestsArray.splice(index, 1);
+                setInternals(internals - 1);
+            }
+            
+            setGuestsArray(updatedGuestsArray);
+            setTotalGuests(totalGuests - 1);
+        };
+    
+        const renderExternalInput = (index) => {
+            setShowExternalInput(true);
+            setActiveInternalGuestIndex(index);
+        };
+    
+        const handleExternalInputChange = (e) => {
+            setExternalInputValue(e.target.value);
+        };
+    
+        const handleExternalInputSubmit = () => {
+            if (externalInputValue.trim()) {
+                const updatedGuestsArray = [...guestsArray];
+                updatedGuestsArray[activeInternalGuestIndex] = {
+                  ...updatedGuestsArray[activeInternalGuestIndex],
+                  external: externalInputValue,
+                };
+                setGuestsArray(updatedGuestsArray);
+                setTotalGuests(totalGuests + 1);
+                setExternals(externals + 1);
+                setExternalInputValue('');
+                setShowExternalInput(false);
+                setActiveInternalGuestIndex(null); 
+            }
+        };
+
+
+        return (
+            <LivePub 
+                guest={guest}
+                guestsArray={guestsArray}
+                internals={internals}
+                externals={externals}
+                totalGuests={totalGuests}
+                showExternalInput={showExternalInput}
+                externalInputValue={externalInputValue}
+                activeInternalGuestIndex={activeInternalGuestIndex}
+                inputChange={inputChange}
+                inputSubmit={inputSubmit}
+                handleDelete={handleDelete}
+                renderExternalInput={renderExternalInput}
+                handleExternalInputChange={handleExternalInputChange}
+                handleExternalInputSubmit={handleExternalInputSubmit}
+                />);
     }
 );
--- src/app/live/livePubView.jsx
+++ src/app/live/livePubView.jsx
@@ -1,98 +1,33 @@
-import {useState} from 'react';
-
 export default function LivePub(props){
-    const[guest, setGuest] = useState('');
-    const[guestsArray, setGuestsArray] = useState([]);
-    const[internals, setInternals] = useState(0);
-    const[externals, setExternals] = useState(0);
-    const[totalGuests, setTotalGuests] = useState(0);
-    const [showExternalInput, setShowExternalInput] = useState(false);
-    const [externalInputValue, setExternalInputValue] = useState('');
-    const [activeInternalGuestIndex, setActiveInternalGuestIndex] = useState(null);
-
-    const inputChange = (e) => {
-        setGuest(e.target.value);
-    };
-
-    const inputSubmit = (e) => {
-        e.preventDefault();
-        if (guest.trim()){
-            setGuestsArray([...guestsArray, {internal: guest, external: null}]);
-            setGuest('');
-            setTotalGuests(totalGuests + 1);
-            setInternals(internals + 1);
-        }
-    };
-
-    const handleDelete = (index) => {
-        const updatedGuestsArray = [...guestsArray];
-
-        if (updatedGuestsArray[index].external) {
-            updatedGuestsArray[index].external = null;
-            setExternals(externals -1);
-        } else {
-            updatedGuestsArray.splice(index, 1);
-            setInternals(internals - 1);
-        }
-        
-        setGuestsArray(updatedGuestsArray);
-        setTotalGuests(totalGuests - 1);
-    };
-
-    const renderExternalInput = (index) => {
-        setShowExternalInput(true);
-        setActiveInternalGuestIndex(index);
-    };
-
-    const handleExternalInputChange = (e) => {
-        setExternalInputValue(e.target.value);
-    };
-
-    const handleExternalInputSubmit = () => {
-        if (externalInputValue.trim()) {
-            const updatedGuestsArray = [...guestsArray];
-            updatedGuestsArray[activeInternalGuestIndex] = {
-              ...updatedGuestsArray[activeInternalGuestIndex],
-              external: externalInputValue,
-            };
-            setGuestsArray(updatedGuestsArray);
-            setTotalGuests(totalGuests + 1);
-            setExternals(externals + 1);
-            setExternalInputValue('');
-            setShowExternalInput(false);
-            setActiveInternalGuestIndex(null); 
-        }
-    };
-
     return(
         <div className="bg-gray-800 min-h-screen p-10">
             <h1 className="font-bold text-center text-5xl">Guest List</h1>
-            <p className="font-bold text-3xl">Total Guests: {totalGuests}</p>
-            <p className="font-bold text-3xl">Total Internal Guests: {internals}</p>
-            <p className="font-bold text-3xl">Total External Guests: {externals}</p>
-            <form onSubmit={inputSubmit}>
+            <p className="font-bold text-3xl">Total Guests: {props.totalGuests}</p>
+            <p className="font-bold text-3xl">Total Internal Guests: {props.internals}</p>
+            <p className="font-bold text-3xl">Total External Guests: {props.externals}</p>
+            <form onSubmit={props.inputSubmit}>
                 <input 
                 type = "text" 
-                value={guest} 
-                onChange={inputChange} 
+                value={props.guest} 
+                onChange={props.inputChange} 
                 placeholder="Add new guest here">
                 </input> 
                 <button className = "simpleButton">Add Internal</button>    
             </form>
-            {showExternalInput && (
+            {props.showExternalInput && (
             <form className="flex items-center justify-left m-2">
-                <input onChange={handleExternalInputChange} value={externalInputValue} placeholder="Add +1" className="px-4 py-2 text-black h- m-2 rounded-full justify-center"/>
-                <button onClick={handleExternalInputSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 w-auto m-2 rounded">Add +1</button>
+                <input onChange={props.handleExternalInputChange} value={props.externalInputValue} placeholder="Add +1" className="px-4 py-2 text-black h- m-2 rounded-full justify-center"/>
+                <button onClick={props.handleExternalInputSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 w-auto m-2 rounded">Add +1</button>
             </form>)}
             <ul className='list-none text-center'>
-                {guestsArray.map((guest, index) => (
+                {props.guestsArray.map((guest, index) => (
                     <li key={index} className='bg-gray-950 flex font-bold justify-left border'>
-                        <button className="bg-red-500 hover:bg-red-700 text-white font-bold px-4 py-2 m-2 rounded w-6 flex justify-center" onClick={() => handleDelete(index)}>x</button>
-                        <button onClick={() => renderExternalInput(index)} className={`bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 m-2 rounded w-6 flex justify-center ${guest.external ? 'cursor-not-allowed opacity-50' : ''}`} disabled={guest.external !==null}>+1</button>
+                        <button className="bg-red-500 hover:bg-red-700 text-white font-bold px-4 py-2 m-2 rounded w-6 flex justify-center" onClick={() => props.handleDelete(index)}>x</button>
+                        <button onClick={() => props.renderExternalInput(index)} className={`bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 m-2 rounded w-6 flex justify-center ${guest.external ? 'cursor-not-allowed opacity-50' : ''}`} disabled={guest.external !==null}>+1</button>
                         <p className='flex items-center justify-left m-2'>{guest.internal}</p>
                         {guest.external && (
                         <div className="flex items-center justify-center mx-auto">
-                            <button className="bg-red-500 hover:bg-red-700 text-white font-bold px-4 py-2 m-2 rounded" onClick={() => handleDelete(index)}>x</button>
+                            <button className="bg-red-500 hover:bg-red-700 text-white font-bold px-4 py-2 m-2 rounded" onClick={() => props.handleDelete(index)}>x</button>
                             <div>+1: </div>
                             <div className="m-2">{guest.external}</div>
                         </div>
@@ -101,7 +36,7 @@ export default function LivePub(props){
                 ))}
             </ul>
 
-            {console.log(guestsArray)}
+            {console.log(props.guestsArray)}
         </div>
     );
 }
